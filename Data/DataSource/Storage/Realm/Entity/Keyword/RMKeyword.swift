9acac0790325efd393dbeff848cf003480da9420
Jinwoo Kim <kidjinwoo@me.com>
2021-08-25 15:24:18 +0900

    style: Obj-C property name은 #keyPath로 대체

---
+++ Data/DataSource/Storage/Realm/Entity/Keyword/RMKeyword.swift
@@ -0,0 +1,37 @@
+//
+//  RMKeyword.swift
+//  Data
+//
+//  Created by Hyoungsu Ham on 2021/08/03.
+//
+
+import Domain
+import RealmSwift
+
+final class RMKeyword: Object {
+    @objc dynamic var text: String = ""
+    @objc dynamic var date: Date = Date()
+    
+    override static func primaryKey() -> String? {
+        return #keyPath(RMKeyword.text)
+    }
+}
+
+extension RMKeyword: DomainConvertibleType {
+    func asDomain() -> Keyword {
+        return Keyword(text: text, date: date)
+    }
+}
+
+extension Keyword: RealmConvertableType {
+    internal var id: String {
+        return text
+    }
+    
+    func asRealm() -> RMKeyword {
+        let keyword = RMKeyword()
+        keyword.text = text
+        keyword.date = date
+        return keyword
+    }
+}
