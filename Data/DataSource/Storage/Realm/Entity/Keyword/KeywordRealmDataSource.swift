ed3c971ba9f5bb93b4b2bb4850182a23a86d3da9
Jinwoo Kim <kidjinwoo@me.com>
2021-08-25 15:46:15 +0900

    refactor: Realm transaction이 끝나야 completed 되도록 수정

---
+++ Data/DataSource/Storage/Realm/Entity/Keyword/KeywordRealmDataSource.swift
@@ -0,0 +1,79 @@
+//
+//  KeywordRealmDataSource.swift
+//  Data
+//
+//  Created by Hyoungsu Ham on 2021/08/03.
+//
+
+import Domain
+import RxSwift
+import RealmSwift
+
+public struct KeywordRealmDataSource: KeywordDataSource {
+
+    public init() {}
+
+    public func getKeywords() -> Observable<[Keyword]> {
+        .create { observer in
+            do {
+                let realm = try Realm()
+                let objects = realm.objects(RMKeyword.self)
+                    .sorted(byKeyPath: #keyPath(RMKeyword.date), ascending: false)
+                let data = Array(objects).compactMap { $0.asDomain() }
+                observer.onNext(data)
+                observer.onCompleted()
+            } catch {
+                dump(error)
+                observer.onError(error)
+            }
+
+            return Disposables.create()
+        }
+    }
+    
+    public func getKeywordsContains(text: String) -> Observable<[Keyword]> {
+        .create { observer in
+            do {
+                let realm = try Realm()
+                let predicate = NSPredicate(format: "\(#keyPath(RMKeyword.text)) CONTAINS[cd] %@", argumentArray: [text as NSString])
+                let objects = realm.objects(RMKeyword.self)
+                    .filter(predicate)
+                    .sorted(byKeyPath: #keyPath(RMKeyword.date), ascending: false)
+                let data = Array(objects)
+                    .compactMap { $0.asDomain() }
+                observer.onNext(data)
+                observer.onCompleted()
+            } catch {
+                dump(error)
+                observer.onError(error)
+            }
+
+            return Disposables.create()
+        }
+    }
+
+    public func saveKeyword(_ keyword: Keyword) -> Observable<Void> {
+        .create { observer in
+            do {
+                let item = keyword.asRealm()
+                let realm = try Realm()
+                
+                let semaphore = DispatchSemaphore(value: 0)
+                
+                try realm.write {
+                    realm.add(item, update: .modified)
+                    semaphore.signal()
+                }
+                
+                semaphore.wait()
+
+                observer.onNext(())
+                observer.onCompleted()
+            } catch {
+                observer.onError(error)
+            }
+
+            return Disposables.create()
+        }
+    }
+}
