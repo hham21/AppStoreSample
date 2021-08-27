f2d0b648f936b0acd35c7c7ba0c9d1aef12c059d
hham21 <hhyoungsu@gmail.com>
2021-08-27 10:48:44 +0900

    refactor: UseCaseImpl 데이터 모듈에서 도메인 모듈로 이동

--- Domain/UseCase/KeywordUseCase.swift
+++ Domain/UseCase/KeywordUseCase.swift
@@ -13,3 +13,24 @@ public protocol KeywordUseCase {
     func getKeywordsContains(text: String) -> Observable<[Keyword]>
     func saveKeyword(_ keyword: String) -> Observable<Void>
 }
+
+public final class KeywordUseCaseImpl: KeywordUseCase {
+    private let repo: KeywordRepository
+    
+    public init(repo: KeywordRepository) {
+        self.repo = repo
+    }
+    
+    public func getKeywords() -> Observable<[Keyword]> {
+        repo.getKeywords()
+    }
+    
+    public func getKeywordsContains(text: String) -> Observable<[Keyword]> {
+        repo.getKeywordsContains(text: text)
+    }
+    
+    public func saveKeyword(_ keyword: String) -> Observable<Void> {
+        let keyword: Keyword = .init(text: keyword, date: Date())
+        return repo.saveKeyword(keyword)
+    }
+}
--- Domain/UseCase/TrackUseCase.swift
+++ Domain/UseCase/TrackUseCase.swift
@@ -11,3 +11,15 @@ import RxSwift
 public protocol TrackUseCase {
     func getTracks(_ query: String) -> Observable<[Track]>
 }
+
+public final class TrackUseCaseImpl: TrackUseCase {
+    private let repo: TrackRepository
+    
+    public init(repo: TrackRepository) {
+        self.repo = repo
+    }
+    
+    public func getTracks(_ query: String) -> Observable<[Track]> {
+        repo.getTracks(query)
+    }
+}
